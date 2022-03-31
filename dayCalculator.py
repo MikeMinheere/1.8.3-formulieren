c6e5333c4ac484569305fd5b0711dac47765d288
Mike Minheere <99067548@mydavinci.nl>
2022-03-31 14:16:17 +0200

    o

--- dayCalculator.py
+++ dayCalculator.py
@@ -1,24 +1,54 @@
-import tkinter
+import tkinter as tk
 from tkinter import ttk
-from calendar import month_name
-from datetime import datetime
+import datetime
+from datetime import date
 
-root = tkinter.Tk()
+def update(arg1,arg2,arg3):
+    global months, maanden
+    dagen.config(values= months[maanden.get()])
+
+months = {
+    'Jan': [i for i in range(1,32)],
+    'Feb': [i for i in range(1,29)],
+    'Mar': [i for i in range(1,32)],
+    'Apr': [i for i in range(1,31)],
+    'May': [i for i in range(1,32)],
+    'Jun': [i for i in range(1,31)],
+    'Jul': [i for i in range(1,32)],
+    'Aug': [i for i in range(1,32)],
+    'Sep': [i for i in range(1,31)],
+    'Okt': [i for i in range(1,32)],
+    'Nov': [i for i in range(1,31)],
+    'Dec': [i for i in range(1,32)]
+}
+
+currentDate = datetime.datetime.now()
+
+root = tk.Tk()
 root.geometry('500x400')
 root.resizable(False, False)
 root.title('days calculator')
 
-label = tkinter.Label(text='date:',font='25')
+label = tk.Label(text='date:',font='25')
 label.grid(column= 1, row= 2)
 label.pack()
 
-maanden = ttk.Combobox(root, textvariable= month_name[2][0:3])
-maanden['values'] = [month_name[i][0:3] for i in range(1,13)]
+#de maanden box
+maandenVar = tk.StringVar()
+maandenVar.trace('w',update)
+maanden = ttk.Combobox(root, values = [i for i in months.keys()],textvariable= maandenVar)
 maanden['state'] = 'readonly'
-maanden.pack(padx=5, pady=50)
+maanden.pack(padx=3, pady=50)
+maanden.set(currentDate.strftime('%b'))
 
-current_month = datetime.now().strftime('%b')
+#de dagen box
+dagen = ttk.Combobox(root, values=[i for i in range(1,32)])
+dagen['state'] = 'readonly'
+dagen.pack(padx=7, pady=50)
+dagen.set(currentDate.strftime('%d'))
 
-maanden.set(current_month)
-
+#de jaren box
+jaarInvullen = tk.StringVar(value = currentDate.strftime('%Y'))
+jaar = tk.Entry(root, textvariable=jaarInvullen)
+jaar.pack(padx=50, pady=50)
 root.mainloop()
