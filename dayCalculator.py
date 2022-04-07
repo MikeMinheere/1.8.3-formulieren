b58b5ff39a77c753c8d160199119528b9a223b00
Mike Minheere <99067548@mydavinci.nl>
2022-04-07 11:43:45 +0200

    daysCalculator v1.0

--- dayCalculator.py
+++ dayCalculator.py
@@ -1,11 +1,37 @@
 import tkinter as tk
 from tkinter import ttk
+import tkinter.messagebox
 import datetime
 from datetime import date
 
+
+#functie om de dagen te updaten
 def update(arg1,arg2,arg3):
-    global months, maanden
-    dagen.config(values= months[maanden.get()])
+    global months, monthsCombo,jaar,dagen
+    if int(jaar.get()) % 4 == 0:
+        months['Feb'] = [i for i in range(1,30)]
+    else: months['Feb'] = [i for i in range(1,29)]
+
+    if int(dagen.get()) > months[monthsCombo.get()][-1]:
+        dagen.current(months[monthsCombo.get()][-2])
+
+    dagen.configure(values= months[monthsCombo.get()])
+
+def calculate():
+    date1 = date.today()
+    date2 = date(int(jaar.get()),list(months.keys()).index(monthsCombo.get()) + 1,int(dagen.get()))
+    deltaDate = date2-date1
+    if deltaDate.days > 0:
+        if deltaDate.days == 1:
+            tkinter.messagebox.showwarning('days calculator', 'dat is morgen')
+        elif deltaDate.days > 1:
+            tkinter.messagebox.showwarning('days calculator', f'dat is {deltaDate.days} dagen in de toekomst')
+    elif deltaDate.days < 0:
+        if deltaDate.days == -1:
+            tkinter.messagebox.showwarning('days calculator', 'dat was gisteren')
+        elif deltaDate.days < -1:
+            tkinter.messagebox.showwarning('days calculator', f'dat is {-deltaDate.days} dagen in het verleden')
+    else: tkinter.messagebox.showwarning('days calculator', 'dat is vandaag')
 
 months = {
     'Jan': [i for i in range(1,32)],
@@ -25,7 +51,7 @@ months = {
 currentDate = datetime.datetime.now()
 
 root = tk.Tk()
-root.geometry('500x400')
+root.geometry('300x200')
 root.resizable(False, False)
 root.title('days calculator')
 
@@ -33,22 +59,27 @@ label = tk.Label(text='date:',font='25')
 label.grid(column= 1, row= 2)
 label.pack()
 
-#de maanden box
-maandenVar = tk.StringVar()
-maandenVar.trace('w',update)
-maanden = ttk.Combobox(root, values = [i for i in months.keys()],textvariable= maandenVar)
-maanden['state'] = 'readonly'
-maanden.pack(padx=3, pady=50)
-maanden.set(currentDate.strftime('%b'))
-
 #de dagen box
 dagen = ttk.Combobox(root, values=[i for i in range(1,32)])
 dagen['state'] = 'readonly'
-dagen.pack(padx=7, pady=50)
+dagen.pack(pady=5)
 dagen.set(currentDate.strftime('%d'))
+
+#de monthsCombo box
+monthsVar = tk.StringVar()
+monthsCombo = ttk.Combobox(root, values = [i for i in months.keys()],textvariable= monthsVar)
+monthsCombo['state'] = 'readonly'
+monthsCombo.pack(pady=5)
+monthsCombo.set(currentDate.strftime('%b'))
 
 #de jaren box
 jaarInvullen = tk.StringVar(value = currentDate.strftime('%Y'))
 jaar = tk.Entry(root, textvariable=jaarInvullen)
-jaar.pack(padx=50, pady=50)
+jaar.pack(pady=5)
+jaarInvullen.trace('w',update)
+monthsVar.trace('w',update)
+
+button = tk.Button(text = 'press to calculate!',command = calculate)
+button.pack(pady=30)
+
 root.mainloop()
